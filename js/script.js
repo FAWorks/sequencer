6de315711c104510cace89d30835c6575964d677
Fredrik Bjurhager <fredrik@faworks.se>
2021-06-22 15:27:36 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-const registerVideo = (bound, video) => {
-	bound = document.querySelector(bound);
-	video = document.querySelector(video);
-	const scrollVideo = ()=>{
-		if(video.duration) {
-			const distanceFromTop = window.scrollY + bound.getBoundingClientRect().top;
-			const rawPercentScrolled = (window.scrollY - distanceFromTop) / (bound.scrollHeight - window.innerHeight);
-			const percentScrolled = Math.min(Math.max(rawPercentScrolled, 0), 1);
-			
-			video.currentTime = video.duration * percentScrolled;
-		}
-		requestAnimationFrame(scrollVideo);
-	}
-	requestAnimationFrame(scrollVideo);
+let $spacer = document.querySelector('.spacer');
+let $video = document.querySelector('.video');
+
+// The height of the spacer element
+let spacerHeight = $spacer.clientHeight;
+// the height of the viewport
+let viewportHeight = document.documentElement.getBoundingClientRect().height;
+
+// We can get the total scrollable height be subtracting the spacer element's height by the viewport height
+let scrollableHeight = spacerHeight - viewportHeight;
+// Get the full duration of the video
+let videoDuration;
+
+// Keep track the video's playtime
+let currentTime = 0;
+
+// The scroll event handler
+function handleScrollEvent(event) {
+  // Here we sync the y position of the scrollbar to the progress of the video
+  currentTime = window.scrollY * videoDuration / scrollableHeight;
+  $video.currentTime = currentTime;
 }
 
-registerVideo("#bound-one", "#bound-one video");
+// Loaded Data handler, that is, the function that runs after the video is ready to play
+function handleLoadedData(event) {
+  // Get the full video duration
+  
+  videoDuration = $video.duration;
+  // Do stuff when user scrolls
+  window.addEventListener('scroll', handleScrollEvent);
+}
+
+// Do stuff when the video is ready to play
+$video.addEventListener('loadeddata', handleLoadedData);
