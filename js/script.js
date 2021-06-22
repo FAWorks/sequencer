c4412e09b6688757b7f69cb7757872f656c03539
Fredrik Bjurhager <fredrik@faworks.se>
2021-06-22 14:26:49 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,48 +1,17 @@
-var canvas;
-var context;
-var photo;
-var timer;
-var count = 000;
-img = new Image();
-canvas = document.getElementById("Canvas1");
-context = canvas.getContext("2d");
-
-img.src = 'img/m3-shot_6-001.png';
-img.onload = function () {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(img, 0, 0, 900, 506);
-};
-
-function StartAnimation() {
-    AnimateNow();
+const registerVideo = (bound, video) => {
+	bound = document.querySelector(bound);
+	video = document.querySelector(video);
+	const scrollVideo = ()=>{
+		if(video.duration) {
+			const distanceFromTop = window.scrollY + bound.getBoundingClientRect().top;
+			const rawPercentScrolled = (window.scrollY - distanceFromTop) / (bound.scrollHeight - window.innerHeight);
+			const percentScrolled = Math.min(Math.max(rawPercentScrolled, 0), 1);
+			
+			video.currentTime = video.duration * percentScrolled;
+		}
+		requestAnimationFrame(scrollVideo);
+	}
+	requestAnimationFrame(scrollVideo);
 }
 
-function AnimateNow() {
-    count++;
-    format = String(count).padStart(3, 0)
-    img.src = 'img/m3-shot_6-' + format + '.png';
-
-    img.onload = function () {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(img, 0, 0, 900, 506);
-    };
-
-    timer = setTimeout(AnimateNow, 41.6666667);
-    if (count >= 131) {
-        StopAnimation();
-    }
-
-}
-
-function StopAnimation() {
-    count = 000;
-    clearTimeout(timer);
-}
-
-function ResetCanvas() {
-    count = 0;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(img, 0, 0, 900, 506);
-    img.src = 'img/m3-shot_6-001.png';
-}
-
+registerVideo("#bound-one", "#bound-one video");
