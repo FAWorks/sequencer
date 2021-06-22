e87e73b334fdcdc268d7d0f07307c43242792531
Fredrik Bjurhager <fredrik@faworks.se>
2021-06-22 11:33:14 +0200

    .

--- script.js
+++ script.js
@@ -3,25 +3,28 @@ var context;
 var photo;
 var timer;
 var count = 000;
+img = new Image();
+canvas = document.getElementById("Canvas1");
+context = canvas.getContext("2d");
+
+img.src = '../img/m3-shot_6-001.png';
+img.onload = function () {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(img, 0, 0, 900, 506);
+};
 
 function StartAnimation() {
-
-    img = new Image();
-    canvas = document.getElementById("Canvas1");
-    context = canvas.getContext("2d");
     AnimateNow();
-    //  img.src = 'images/image1.JPG';
 }
 
 function AnimateNow() {
     count++;
     format = String(count).padStart(3, 0)
     img.src = '../img/m3-shot_6-' + format + '.png';
-    
-    img.onload = function()
-    {
+
+    img.onload = function () {
         context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(img,0,0,500,281);    
+        context.drawImage(img, 0, 0, 900, 506);
     };
 
     timer = setTimeout(AnimateNow, 41.6666667);
@@ -39,4 +42,7 @@ function StopAnimation() {
 function ResetCanvas() {
     count = 0;
     context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(img, 0, 0, 900, 506);
+    img.src = '../img/m3-shot_6-001.png';
 }
+
